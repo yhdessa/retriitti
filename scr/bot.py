bf1b26d599cb4ec8e414a09e35c1f5d7dd2335ad
gtb <d2@d2s-MacBook-Air.local>
2025-11-26 20:59:20 +0200

    minor fixes

--- scr/bot.py
+++ scr/bot.py
@@ -1,8 +1,10 @@
 import asyncio
 import os
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, html, Router
 from aiogram.client.default import DefaultBotProperties
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -12,6 +14,14 @@ bot = Bot(
     default=DefaultBotProperties(parse_mode="HTML")
 )
 dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+@dp.message()
+async def echo_handler(message: "help"):
+    await message.answer(f"Hi! This bot was created for sharing music!")
 
 @dp.message()
 async def echo(message: types.Message):
